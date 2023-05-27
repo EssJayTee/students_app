620c55c85c00270236ab0f17e49aee120976c6d7
EssJayTee <szymontaramasz@gmail.com>
2023-05-27 23:24:28 +0200

    post z podstawowymi danymi studenta

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class StudentCreateSchema(BaseModel):
+    first_name: str
+    last_name: str
 
 app = FastAPI()
 
+@app.post("/students")
+async def create_student(student: StudentCreateSchema):
+    return student
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "JD"}
